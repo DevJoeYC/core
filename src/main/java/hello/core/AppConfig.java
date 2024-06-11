f75d15dfc0aa54bd8e5e49383812374b5b5ad832
조윤찬 <qweaszx2122@gmail.com>
2024-06-11 16:59:40 +0900

    company

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,20 +10,26 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Random;
+import java.util.Scanner;
+
 @Configuration
 public class AppConfig {
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemoryMemberRepository memberRepository() {
+    public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
@@ -31,4 +37,6 @@ public class AppConfig {
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
+
+
 }
